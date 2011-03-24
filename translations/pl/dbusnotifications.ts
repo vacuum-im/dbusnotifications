c2b6bba79d17350d1539dafdddb72be040d77fcd
Crying Angel <cangel@inbox.ru>
2011-03-24 13:10:16 +0200

    CMake build system

---
+++ translations/pl/dbusnotifications.ts
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" sourcelanguage="en">
+<context>
+    <name>DbusPopupHandler</name>
+    <message>
+        <source>Dbus Popup Notifications Handler</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Allows other modules use DBus to show notifications</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ignore</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ translations/ru/dbusnotifications.ts
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU" sourcelanguage="en">
+<context>
+    <name>DbusPopupHandler</name>
+    <message>
+        <source>Dbus Popup Notifications Handler</source>
+        <translation>DBus уведомления</translation>
+    </message>
+    <message>
+        <source>Allows other modules use DBus to show notifications</source>
+        <translation>Позволяет использовать DBus для вывода уведомлений</translation>
+    </message>
+    <message>
+        <source>Show</source>
+        <translation>Показать</translation>
+    </message>
+    <message>
+        <source>Ignore</source>
+        <translation>Игнорировать</translation>
+    </message>
+</context>
+</TS>
